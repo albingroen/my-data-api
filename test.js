2c943592dcfefffaca93a93f58fcc512ff73a9cc
Pietu <pietu@smartly.io>
2018-11-24 10:17:43 +0200

    mailClient fetches domains and dates

---
+++ test.js
@@ -0,0 +1,75 @@
+'use strict';
+const {OAuth2Client} = require('google-auth-library');
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
+const opn = require('opn');
+const {google} = require('googleapis');
+// Download your OAuth2 configuration from the Google
+const keys = require('./client_id.json');
+
+const MailClient = require('./mailClient.js');
+
+/**
+ * Start by acquiring a pre-authenticated oAuth2 client.
+ */
+async function main() {
+  try {
+    const oAuth2Client = await getAuthenticatedClient();
+    console.log('fetching domains form mail');
+    const mailClient = new MailClient(oAuth2Client);
+    const result = await mailClient.scrape();
+    console.log(result);
+  } catch (e) {
+    console.error(e);
+  }
+  process.exit();
+}
+
+/**
+ * Create a new OAuth2Client, and go through the OAuth2 content
+ * workflow.  Return the full client to the callback.
+ */
+function getAuthenticatedClient() {
+  return new Promise((resolve, reject) => {
+    // create an oAuth client to authorize the API call.  Secrets are kept in a `keys.json` file,
+    // which should be downloaded from the Google Developers Console.
+    const oAuth2Client = new OAuth2Client(
+      keys.web.client_id,
+      keys.web.client_secret,
+      keys.web.redirect_uris[0]
+    );
+
+    // Generate the url that will be used for the consent dialog.
+    const authorizeUrl = oAuth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: 'https://www.googleapis.com/auth/gmail.readonly',
+    });
+
+    // Open an http server to accept the oauth callback. In this simple example, the
+    // only request to our webserver is to /oauth2callback?code=<code>
+    const server = http
+      .createServer(async (req, res) => {
+        if (req.url.indexOf('/oauth2callback') > -1) {
+          // acquire the code from the querystring, and close the web server.
+          const qs = querystring.parse(url.parse(req.url).query);
+          console.log(`Code is ${qs.code}`);
+          res.end('Authentication successful! Please return to the console.');
+          server.close();
+
+          // Now that we have the code, use that to acquire tokens.
+          const r = await oAuth2Client.getToken(qs.code);
+          // Make sure to set the credentials on the OAuth2 client.
+          oAuth2Client.setCredentials(r.tokens);
+          console.info('Tokens acquired.');
+          resolve(oAuth2Client);
+        }
+      })
+      .listen(8080, () => {
+        // open the browser to the authorize url to start the workflow
+        opn(authorizeUrl);
+      });
+  });
+}
+
+main();
